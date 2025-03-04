35ed853869af93e29ba823467f73d2e37842f986
Gabriel Nodari Pereira <144969599+GabeNodari@users.noreply.github.com>
2025-03-04 14:29:26 -0300

    main.js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
+//Declarações
 const MAX_TENTATIVAS = 5;
 const NUMERO_SECRETO = Math.floor(Math.random() * 51);
-
 let numeroChutes = 0;
 let acertou = false;
 
-//Verifica se o chute foi correto e retorna true se acertou ou uma dica (número dever ser maior ou menor)//
+//Verifica se o chute foi correto; retorna dica
 function verificaChute(chute, numeroSecreto, tentativasRestantes) {
   if (chute === numeroSecreto) {
     alert('Acertou! :)');
     return true;
   } 
-  const dica = chute > numeroSecreto ? 'menor' : 'maior';
-  alert(`Errou! :( \nO número secreto é ${dica} do que você colocou! \nVocê tem ${tentativasRestantes} tentativas restantes.`);
+  const DICA = chute > numeroSecreto ? 'menor' : 'maior';
+  alert(`Errou! :( \nO número secreto é ${DICA} do que você colocou! \nVocê tem ${tentativasRestantes} tentativas restantes.`);
   return false;
 }
 
-//Exibe o prompt com as instruções; valida o input; atualiza o número de tentativas; verifica se acertou ou exibe o número secreto ao final//
+//Loop do jogo
 while (!acertou && numeroChutes < MAX_TENTATIVAS) {
-  const chute = parseInt(prompt('Digite um número entre 1 e 50: '), 10);
-  if (isNaN(chute)) {
+  const CHUTE = parseInt(prompt('Digite um número entre 1 e 50: '), 10);
+  if (isNaN(CHUTE)) {
     alert('Por favor, insira um número válido!');
     continue;
   }
   numeroChutes++;
-  acertou = verificaChute(chute, NUMERO_SECRETO, MAX_TENTATIVAS - numeroChutes);
+  acertou = verificaChute(CHUTE, NUMERO_SECRETO, MAX_TENTATIVAS - numeroChutes);
 }
 if (!acertou) {
   alert(`Número máximo de tentativas alcançado!\nO número secreto era: ${NUMERO_SECRETO}`);
